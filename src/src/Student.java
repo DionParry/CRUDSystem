e17f0a46b1a6b77725e59ff8d0bd22ffbc0ef665
DionParry <dionjohnparry@gmail.com>
2019-03-10 14:37:23 +0000

    Updated the student class to include the set and get methods.

--- src/src/Student.java
+++ src/src/Student.java
@@ -1,5 +1,34 @@
 public class Student {
-    public static void main(String[] args) {
-        System.out.println("Chink");
+
+    public Student(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurnname(String s) {
+        surname = s;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String a) {
+        age = a;
+    }
+
+
 }
