a6a405cc557c5db71c7a8ffd94d431ea8d8724c4
craigowen <craig-owen@hotmail.com>
2019-03-10 14:47:22 +0000

    Add DB Class

---
+++ src/src/DBManager.java
@@ -0,0 +1,32 @@
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ * Connect to Database
+ * @author Craig Owen
+ */
+public class DBManager {
+    public static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    public static final String USER = "testuser";
+    public static final String PASS = "testpass";
+    /**
+     * Get a connection to database
+     * @return Connection object
+     */
+    public static Connection getConnection()
+    {
+        try {
+            DriverManager.registerDriver(new Driver());
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
+    }
+    /**
+     * Test Connection
+     */
+    public static void main(String[] args) {
+        Connection connection = connectionFactory.getConnection();
+    }
+}
